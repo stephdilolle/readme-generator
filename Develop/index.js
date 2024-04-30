e6b765d5e9cba1040e6ffb2296e2c218166de42d
sdilolle <stephdilolle@gmail.com>
2024-04-30 00:58:33 -0400

    updated index.js file with readme generator and updated my readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,99 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'projectTitle',
+            message: 'Enter the title of your project:'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a description for your project:'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Enter installation instructions:'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter usage information:'
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Enter contribution guidelines:'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Enter test instructions:'
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Enter the link to your GitHub profile and your email address:'
+        }
+    ]);
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function generateReadmeContent(answers) {
+    return `
+# ${answers.projectTitle}
+
+## Description
+
+${answers.description}
+
+## Installation
+
+${answers.installation}
+
+## Usage
+
+${answers.usage}
+
+## Contributing
+
+${answers.contributing}
+
+## Tests
+
+${answers.tests}
+
+## Questions
+
+${answers.questions}
+`;
+}
+
+function writeToReadme(content) {
+    fs.writeFile('README.md', content, (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('README.md file has been created!');
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    try {
+        const userResponses = await promptUser();
+        const readmeContent = generateReadmeContent(userResponses);
+        writeToReadme(readmeContent);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 // Function call to initialize app
 init();
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# readme-generator
+
+# README Generator
+
+## Description
+
+A shortcut to creating a high quality README file using Node.js
+
+## Installation
+
+User must have Node.js installed with Inquirer version 8.2.4
+
+## Usage
+
+Use this program to create a README file.
+Link to video example:
+
+## Contributing
+
+N/A
+
+## Tests
+
+N/A
+
+## Questions
+
+GitHub: https://github.com/stephdilolle Email: stephdilolle@gmail.com
